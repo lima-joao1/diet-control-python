21f409b806eb7cb437567bbbe255e5b4800a1e55
joão <joaovitorlimadearaujo@gmail.com>
2026-03-19 11:59:44 -0300

    Delete ex10.py

--- ex10.py
+++
@@ -1,5 +0,0 @@
-leftEdge = int(input("Primeiro número: "))
-rightEdge = int(input("Segundo número: "))
-
-for i in range(leftEdge, rightEdge + 1): # Considerando intervalo fechado nos dois lados
-    print(i, end=" ")
