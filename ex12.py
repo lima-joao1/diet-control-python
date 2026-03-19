14d49d5dabddee0d8b6453453cde28bd35ec2e65
joão <joaovitorlimadearaujo@gmail.com>
2026-03-19 12:00:04 -0300

    Delete ex12.py

--- ex12.py
+++
@@ -1,6 +0,0 @@
-numberTabuada = int(input("Número da tabuada: "))
-
-for i in range(1, 11):
-    result =  (numberTabuada * i)
-    output = "%d X %d = %d" % (numberTabuada, i, result)
-    print(output)
